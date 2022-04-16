417015caea8c48c961bd5459d95baca6e99ab0e4
Tinkerer <46686760+TinkeringButReallyPlayingWithFire@users.noreply.github.com>
2022-04-16 20:20:56 +0100

    Changes within changes
    :)

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,15 +1,16 @@
 // Step 1: Import React
 import * as React from 'react'
+import {Link} from 'gatsby'
 import Layout from '../components/layout'
-// Step 2: Define your component
-const IndexPage = () => {
-  return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following Gatsby noaka</p>
-    </Layout>
 
+// Step 2: Define your component
+const AboutPage = () => {
+  return (
+      <Layout>
+        <p>Hi there dis be a kid akakak</p>
+      </Layout>
   )
 }
 
 // Step 3: Export your component
-export default IndexPage
+export default AboutPage
